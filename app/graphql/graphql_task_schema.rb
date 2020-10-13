813797a1c7f401a4c6e90a0f803b239851ecd712
yuya yamamoto <yuya.yamamoto2000@gmail.com>
2020-10-13 17:29:19 +0900

    GraphQL仮実装

---
+++ app/graphql/graphql_task_schema.rb
@@ -0,0 +1,4 @@
+GraphqlTaskSchema = GraphQL::Schema.define do
+  mutation(Types::MutationType)
+  query(Types::QueryType)
+end
---
+++ app/graphql/types/post_type.rb
@@ -0,0 +1,6 @@
+Types::PostType = GraphQL::ObjectType.define do
+  name "Post"
+  field :id, !types.ID
+  field :title, !types.String
+  field :details, !types.String
+end
